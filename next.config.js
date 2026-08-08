18d7b268409f0047c91db56c6c84f50af4819d28
china.Mr <china.ra0.cn@gmail.com>
2026-08-08 10:00:21 +0800

    [Docker] Add standalone output to next.config.js for yushitou

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     unoptimized: true,
     remotePatterns: [
-      { protocol: "http", hostname: "strapi.ra0.cn" },
-      { protocol: "https", hostname: "strapi.ra0.cn" },
+      { protocol: "http", hostname: "payload.ra0.cn",
+      },
+      {
+        protocol: "http",
+        hostname: "strapi.ra0.cn" },
+      { protocol: "https", hostname: "payload.ra0.cn",
+      },
+      {
+        protocol: "http",
+        hostname: "strapi.ra0.cn" },
     ],
   },
   compress: true,
